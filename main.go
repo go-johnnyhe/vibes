50b33ccabff5ba0f7f419f7fb6b3cb81560a0271
dimentio233 <johnnyhe7@outlook.com>
2025-05-25 23:30:53 -0700

    fixed import name to vibes.

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/go-johnnyhe/weather/cmd"
+import "github.com/go-johnnyhe/vibes/cmd"
 
 func main() {
 	cmd.Execute()
