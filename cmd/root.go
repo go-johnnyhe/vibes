e4339687a72bba1fcffa2679830813dbfbf00306
dimentio233 <johnnyhe7@outlook.com>
2025-05-27 00:19:00 -0700

    no actual change, tryna trigger flag update.

--- cmd/root.go
+++ cmd/root.go
@@ -399,15 +399,8 @@ func Execute() {
 	}
 }
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.Flags().StringVarP(&unitFlag, "unit", "u", "fahrenheit", "Temperature unit: 'celsius'/'c' or 'fahrenheit'/'f'")
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.weather.yaml)")
 	rootCmd.Flags().IntVarP(&durationFlag, "hours", "d", 4, "Number of hours you'd like to forecast")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 
